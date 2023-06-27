f01f1aac978dc40d4e58d76c5a4ac614b0cbea9c
Nick <71001714+nrkorte@users.noreply.github.com>
2023-06-27 13:47:32 -0600

    Update add_office_location.ps1

--- add_office_location.ps1
+++ add_office_location.ps1
@@ -1,17 +1,17 @@
 Import-Module ActiveDirectory
 
-$ous = "OU=Users,OU=COL,OU=CP,OU=_Divisions,DC=icig,DC=global", "OU=Users,OU=BOU,OU=CP,OU=_Divisions,DC=icig,DC=global"
+$ous = "OU=Users,OU=COL,OU=CP,OU=_Divisions,DC=domain,DC=global", "OU=Users,OU=BOU,OU=CP,OU=_Divisions,DC=domain,DC=global"
 
 $users = $ous | ForEach-Object { Get-ADUser -SearchBase $_ -Filter * -Properties DistinguishedName | Select-Object -expand DistinguishedName }
 $sams = $ous | ForEach-Object { Get-ADUser -SearchBase $_ -Filter * -Properties SamAccountName | Select-Object -expand SamAccountName }
 $i = 0
 foreach ($usert in $users) {
     $OU = ($usert -split ',OU=')[2]
-    if ($OU -eq "COL") {
-        Set-ADUser -Identity $sams[$i] -Office "COL"
+    if ($OU -eq "gp1") {
+        Set-ADUser -Identity $sams[$i] -Office "gp1"
     }
-    elseif ($OU -eq "BOU") {
-        Set-ADUser -Identity $sams[$i] -Office "BOU"
+    elseif ($OU -eq "gp2") {
+        Set-ADUser -Identity $sams[$i] -Office "gp2"
     }
     else {
         Write-Host "There was an error retrieving user location for " ($usert -split ',OU=')[0]
